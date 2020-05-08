302e48af6bff254d7b79915df060d1f70696e37b
Tom Galloway <tagallow@outlook.com>
2020-05-08 09:31:16 -0500

    Added a cleaner version of Sieve of Eratosthenes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Linq;
+using System.Diagnostics;
 using System;
 
 namespace EulerCore2
@@ -10,12 +11,13 @@ namespace EulerCore2
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            SumSquareDifference.Solve();
+            var p = Tools.GetPrimes();
 
+            sw.Stop();
+            Console.WriteLine("sieve2 done, {0} items", p.Count);
             Console.WriteLine("Time Elapsed: {0:n0} minutes {1:n0} seconds {2:n0} milliseconds",
                 sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds);
 
-            sw.Reset();
         }
     }
 }
--- Tools/Tools.cs
+++ Tools/Tools.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -5,33 +6,66 @@ namespace EulerCore2
 {
     static class Tools
     {
-        public static List<int> GetPrimes10k()
+        public static List<int> GetPrimes(int max = 104730)
         {
-            return SieveEratosthenes(104730);
+            return SieveEratosthenes2(max);
         }
-        public static List<int> GetPrimes(int max = 7920)
+        private static List<int> SieveEratosthenes2(int max)
         {
-            return SieveEratosthenes(max);
+            bool[] sieve = new bool[max];
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < max; i++)
+            {
+                sieve[i] = true;
+            }
+            sieve[0] = sieve[1] = false;
+            int p = 2;
+            while (Math.Pow(p, 2) <= max)
+            {
+                for (int i = (p + p); i < max; i += p)
+                {
+                    sieve[i] = false;
+                }
+                for (int i = p; i < max; i++)
+                {
+                    if (sieve[i] && i > p)
+                    {
+                        p = i;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < max; i++)
+            {
+                if (sieve[i])
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
         }
-        private static List<int> SieveEratosthenes(int max)
+
+        #region unused
+        /* private static List<int> SieveEratosthenes(int max)
         {
             bool[] N = new bool[max];
-            for (int i = 0; i < N.Length; i++)
+            for (int i = 0; i < max; i++)
                 N[i] = true;
             N[0] = N[1] = false;
 
-            for (int i = 2; i < N.Length; i++)
+            for (int i = 2; i < max; i++)
             {
                 if (N[i])
                 {
-                    for (int j = (2 * i); j < N.Length; j += i)
+                    for (int j = (2 * i); j < max; j += i)
                     {
                         N[j] = false;
                     }
                 }
             }
             List<int> result = new List<int>();
-            for (int i = 0; i < N.Length; i++)
+            for (int i = 0; i < max; i++)
             {
                 if (N[i])
                 {
@@ -40,19 +74,18 @@ namespace EulerCore2
             }
             return result;
         }
-        #region unused
-        // private static int[] GetPrimesFile()
-        // {
-        //     string[] file = File.ReadAllLines(@"Tools\1kprimes.txt")[0].Split(',');
-        //     int[] primes = Array.ConvertAll(file, int.Parse);
-        //     return primes;
-        // }
-        // private static int[] GetPrimes10kFile()
-        // {
-        //     string[] file = File.ReadAllLines(@"Tools\10kprimes.txt")[0].Split(',');
-        //     int[] primes = Array.ConvertAll(file, int.Parse);
-        //     return primes;
-        // }
+        private static int[] GetPrimesFile()
+        {
+            string[] file = File.ReadAllLines(@"Tools\1kprimes.txt")[0].Split(',');
+            int[] primes = Array.ConvertAll(file, int.Parse);
+            return primes;
+        }
+        public static int[] GetPrimes10kFile()
+        {
+            string[] file = File.ReadAllLines(@"Tools\10kprimes.txt")[0].Split(',');
+            int[] primes = Array.ConvertAll(file, int.Parse);
+            return primes;
+        } */
         #endregion
 
     }
