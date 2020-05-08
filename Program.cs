b8ffd98d8dd79ed7d32d8c5f2a03bd9913f43f82
Tom Galloway <tagallow@outlook.com>
2020-05-07 20:38:12 -0500

    Initial commit with project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System;
+
+namespace EulerCore2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Stopwatch sw = new Stopwatch();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            
+            Console.WriteLine("Hello World!");
+            
+            sw.Stop();
+            Console.WriteLine("Time Elapsed: {0} seconds", sw.Elapsed.TotalSeconds);
+        }
+    }
+    interface IPuzzle
+    {
+        void Solve();
+    }
+}
